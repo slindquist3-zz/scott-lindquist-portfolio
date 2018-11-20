c14e2cb7249af44c2f6c3b06a15e1b65388a7005
Scott Lindquist <scott.lindquist@equinox.com>
2018-11-20 15:38:26 -0500

    With HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
+
 
 import SlidesContainer from './components/main/SlidesContainer.js';
 import Archive from './components/main/Archive.js';
@@ -8,13 +9,13 @@ import Archive from './components/main/Archive.js';
 import './index.scss';
 
 const routing = (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <div className="App-Container">
       <Route exact path="/" component={SlidesContainer}/>
       <Route path="/(1|2|3)/" component={SlidesContainer}/>
       <Route exact path="/archive" component={Archive} />
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(routing, document.getElementById('root'));
