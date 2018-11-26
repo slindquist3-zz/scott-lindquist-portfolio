147aa6e43d8f35df803ed77decf1660f63a57e89
Scott Lindquist <scott.lindquist@equinox.com>
2018-11-26 11:32:04 -0500

    about bio

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,12 @@
 import React from 'react'
 function About() {
-    return <h1 className="About">About</h1>
+    return (
+      <div className="About">
+        <h1 className="header">About me</h1>
+        <p>I'm a front end engineer living and working in NYC. I've worked for brands like Bloomindales and Equinox to create contemporary, responsive user experiences.</p>
+        <p>Check out my work!</p>
+
+      </div>
+    )
 }
 export default About
