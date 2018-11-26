c2ae5f01fcb515a704df34cf7f76a2431fb4a621
Scott Lindquist <scott.lindquist@equinox.com>
2018-11-26 14:29:06 -0500

    edit title and about me

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,7 @@ function About() {
     return (
       <div className="About">
         <h1 className="header">About me</h1>
-        <p>I'm a front end engineer living and working in NYC. I've worked for brands like Bloomindales and Equinox to create contemporary, responsive user experiences.</p>
+        <p>I'm a Front End Engineer living and working in NYC. I've developed sites for companies Bloomindales, Equinox, and American Express to create contemporary, responsive user experiences with tools like React.js.</p>
         <p>Check out my work!</p>
 
       </div>
